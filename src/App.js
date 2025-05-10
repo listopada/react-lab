ce94244dc3ad85fea93eb585c01ac2c4378e2b8b
listopada <listopad@agh.edu.pl>
2025-05-10 14:42:26 +0200

    Add reactivity

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
+import { useState } from "react";
+
 function App() {
-    let email = 'listopad@agh.edu.pl';
+    const [email, setEmail] = useState('listopad@agh.edu.pl');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let message;
+    if (email.length < 8) {
+        message = 'Adres e-mail jest zbyt krótki.';
+    } else if (email.length > 20) {
+        message = 'Adres e-mail jest zbyt długi.';
+    } else {
+        message = 'Adres e-mail wygląda poprawnie.';
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>Twój e-mail to {email}</h2>
+            <input type="text" value={email} onChange={handleChange}/>
+            <div>{message}</div>
         </div>
     );
 }
